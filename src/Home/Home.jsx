1ff4ae034605ebb7d24f5415eb7b4f16936e8e3b
DoumbiaAwa <awa80598@gmail.com>
2024-10-25 22:01:09 +0000

    menu

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,32 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Home.css'
-export default function Home() {
-  return (
-    <div>
-    <div id='home'>
-        <img src="./images/femme-tenant.png" alt="" className='femme' />
-{/* navbar start */}
-<nav className="navbar navbar-expand-lg">
-  <div className='logo'>
-    <img src="./images/logo.png" alt="" />
-  </div>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
-      <a  href="#">Accueil</a>
-      <a  href="#">A Propos</a>
-      <a  href="#">Services</a>
-      <a  href="#">Projet</a>
-      <button className='btn-contact'>
-      Contact
-      </button>
-    </div>
-  </div>
-</nav>
-{/* navbar end */}
+import { motion } from "framer-motion";
 
+export default function Home() {
+    const [color, setColor] = useState(false);
+
+    const changeColor = () => {
+      if (window.scrollY >= 90) {  // Utiliser scrollY pour obtenir la position verticale
+        setColor(true);
+      } else {
+        setColor(false);
+      }
+    };
+    
+    useEffect(() => {
+      window.addEventListener('scroll', changeColor);
+      return () => window.removeEventListener('scroll', changeColor); // Nettoyage de l'event listener
+    }, []);
+    
+    return (
+      <div>
+        <div id='home'>
+          <img src="./images/femme-tenant.png" alt="" className='femme' />
+          
+          {/* navbar start */}
+          <nav className="navbar navbar-expand-lg">
+           
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className={color ? 'menu-bar' : 'menu-bar-bg'}>
+              <div className='logo' style={{marginRight: '600px'}}>
+              <img src="./images/logo.png" alt="" />
+            </div>
+          
+                 <li href="#">Accueil</li>
+                <li href="#">A Propos</li>
+                <li href="#">Services</li>
+                <li href="#">Projet</li>
+                <motion.div 
+                  whileHover={{ scale: 1.2 }}
+                  whileTap={{ scale: 0.9 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <button className='btn-contact'>
+                    Contact
+                  </button>
+                </motion.div>
+            </div>
+                
+              </div>
+          </nav>
+          {/* navbar end */}
 {/* contact start */}
 <div className='contact-container'>
 <h1>Agriculture Information Système</h1>
@@ -37,9 +63,17 @@ export default function Home() {
     <input type="text"  placeholder='Numero'/>
     <textarea name="" id="" placeholder='message'></textarea>
 
+<motion.div
+ whileHover={{ scale: 1.2 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+>
 <button className='btn-form'>
     Soumettre
 </button>
+
+</motion.div>
+
 </form>
 </div>
 {/* contact end */}
@@ -60,9 +94,17 @@ export default function Home() {
 Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum. Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.
 <br />
 <br />
+<motion.div
+ whileHover={{ scale: 1.2 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
 <button className='btn-about'>
 Decouvrer nos equipes 
 </button>
+</motion.div>
+
 </p>
 </div>
 </div>
@@ -70,31 +112,60 @@ Decouvrer nos equipes
 {/* equipes start */}
 <div id='equipe'>
 
+<motion.div
+ whileHover={{ scale: 1.2 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
+<div className='equipe-div'>
+    <img src="./images/Group 8.png" alt=""  className='eq'/>
+    <h3>Nom</h3>
+    <p>Proffesion</p>
+ </div>
+</motion.div>
+ 
+<motion.div
+ whileHover={{ scale: 1.2 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
+
+ <div className='equipe-div'>
+    <img src="./images/Group 8.png" alt=""  className='eq'/>
+    <h3>Nom</h3>
+    <p>Proffesion</p>
+ </div>
+ </motion.div>
+
+ <motion.div
+ whileHover={{ scale: 1.2 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
+<div className='equipe-div'>
+    <img src="./images/Group 8.png" alt=""  className='eq'/>
+    <h3>Nom</h3>
+    <p>Proffesion</p>
+ </div>
+
+</motion.div>
+ 
+<motion.div
+ whileHover={{ scale: 1.2 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
  <div className='equipe-div'>
     <img src="./images/Group 8.png" alt=""  className='eq'/>
     <h3>Nom</h3>
     <p>Proffesion</p>
  </div>
 
- <div className='equipe-div'>
-    <img src="./images/Group 8.png" alt=""  className='eq'/>
-    <h3>Nom</h3>
-    <p>Proffesion</p>
- </div>
-
- <div className='equipe-div'>
-    <img src="./images/Group 8.png" alt=""  className='eq'/>
-    <h3>Nom</h3>
-    <p>Proffesion</p>
- </div>
-
- <div className='equipe-div'>
-    <img src="./images/Group 8.png" alt=""  className='eq'/>
-    <h3>Nom</h3>
-    <p>Proffesion</p>
- </div>
-
-
+ </motion.div>
 </div>
 {/* equipes end */}
 </div>
@@ -107,7 +178,13 @@ Decouvrer nos equipes
     <h2 className='title-service'>Nos Services</h2>
 
     {/* card 1 */}
-    <div className='service-container'>
+    <motion.div
+ whileHover={{ scale: 1.1 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
+<div className='service-container'>
     <div class='service-icon'>
             <i class="fas fa-globe fa-3x"></i>
         </div>
@@ -116,8 +193,16 @@ Decouvrer nos equipes
             Nous proposons des solutions complètes et personnalisées pour la création de sites web, en fonction des besoins spécifiques de votre entreprise. Que vous ayez besoin d'un site vitrine, d'une boutique en ligne ou d'une plateforme complexe, nous garantissons des designs modernes, intuitifs et entièrement responsive. De plus, nous concevons des logos professionnels qui reflètent l'identité de votre marque, ainsi que des spots publicitaires percutants pour renforcer votre visibilité en ligne.
         </div>
     </div>
+</motion.div>
+   
 {/* card 2 */}
 
+<motion.div
+ whileHover={{ scale: 1.1 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
 <div className='service-container'>
     <div class='service-icon'>
             <i class="fas fa-tractor fa-3x"></i>
@@ -127,8 +212,14 @@ Decouvrer nos equipes
             Nous simplifions la gestion des exploitations agricoles grâce à des applications personnalisées et faciles à utiliser. Nos solutions numériques permettent aux agriculteurs de gérer efficacement leurs cultures, leurs stocks et leurs opérations quotidiennes, tout en améliorant leur productivité. Grâce à nos outils, les agriculteurs peuvent suivre leurs activités en temps réel et prendre des décisions informées pour optimiser leurs ressources.        </div>
     </div>
 
-
+    </motion.div>
 {/* card 3 */}
+<motion.div
+ whileHover={{ scale: 1.1 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
 <div className='service-container'>
     <div class='service-icon'>
             <i class="fas fa-mobile-alt fa-3x"></i>
@@ -137,7 +228,16 @@ Decouvrer nos equipes
             <h3>Création d'applications web et mobiles.</h3> 
             Nous développons des applications web et mobiles sur mesure, dotées d'une interface intuitive et d'une expérience utilisateur optimisée. Nos solutions permettent aux entreprises d'améliorer leur efficacité opérationnelle, en automatisant les tâches et en facilitant la gestion quotidienne. Grâce à nos applications performantes et faciles à utiliser, vous pouvez vous concentrer sur la croissance de votre entreprise tout en offrant à vos utilisateurs une expérience fluide et engageante.        </div>
     </div>
+
+</motion.div>
+
 {/* card 4 */}
+<motion.div
+ whileHover={{ scale: 1.1 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
 <div className='service-container'>
     <div class='service-icon'>
             <i class="fas fa-cogs fa-3x"></i>
@@ -147,6 +247,8 @@ Decouvrer nos equipes
             Nous effectuons des analyses approfondies de vos systèmes pour identifier les besoins spécifiques de votre entreprise et optimiser vos processus existants. En évaluant l'ensemble de votre infrastructure, nous mettons en lumière les points d'amélioration et les opportunités de croissance. De plus, notre expertise en analyse des données vous permet d'extraire des insights précieux pour prendre des décisions éclairées, améliorer l'efficacité opérationnelle et renforcer votre position sur le marché.
      </div>
     </div>
+
+</motion.div>
 
 </div>
 
@@ -170,7 +272,15 @@ Decouvrer nos equipes
 Koumi est une application facilitant la mise en relation entre les différents acteurs du système agricole.
 
 </p>
+<motion.div
+ whileHover={{ scale: 1.1 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
 <button className='btn-projet'>Voir plus</button>
+
+</motion.div>
 
 </div>
 
@@ -180,7 +290,15 @@ Koumi est une application facilitant la mise en relation entre les différents a
 Koumi est une application facilitant la mise en relation entre les différents acteurs du système agricole.
 
 </p>
+<motion.div
+ whileHover={{ scale: 1.1 }}  // Zoom à 120% lors du survol
+ whileTap={{ scale: 0.9 }}    // Réduction à 90% lors du clic
+ transition={{ duration: 0.3 }}  // Durée de l'animation
+ 
+>
 <button className='btn-projet'>Voir plus</button>
+
+</motion.div>
 
 </div>
 
